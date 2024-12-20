a89d471637339066595ab8f8c4f485ba66b6f1f3
M-Elv1n <e.musayev590@gmail.com>
2024-12-20 15:32:18 +0100

    fix time script

--- src/App.js
+++ src/App.js
@@ -1,3 +1,168 @@
+// import React, { Suspense } from "react";
+// import { Routes, Route } from "react-router-dom";
+// import { Helmet } from "react-helmet";
+// import Home from "./pages/Home/Home";
+// import Loader from "./components/Loader"; // Заглушка для Suspense
+// import "./css/styles.scss";
+
+// const GoogleTagManager = () => (
+//   <Helmet>
+//     {/* Добавляем скрипт Google Tag Manager */}
+//     <script
+//       async
+//       src="https://www.googletagmanager.com/gtag/js?id=AW-16807421415"
+//     ></script>
+//     <script>
+//       {`
+//         window.dataLayer = window.dataLayer || [];
+//         function gtag(){dataLayer.push(arguments);}
+//         gtag('js', new Date());
+//         gtag('config', 'AW-16807421415');
+//       `}
+//     </script>
+//   </Helmet>
+// );
+
+// // Загружаем компоненты лениво
+// const TimeLocationChecker = React.lazy(() =>
+//   import("./components/TimeLocationChecker")
+// );
+// const ProductList = React.lazy(() =>
+//   import("./pages/Home/Products/Man/ProductListMan")
+// );
+// const ProductDetails = React.lazy(() =>
+//   import("./pages/Home/Products/Man/ProductDetailsMan")
+// );
+// const ProductListWomen = React.lazy(() =>
+//   import("./pages/Home/Products/Woman/ProductListWoman")
+// );
+// const ProductDetailsWomen = React.lazy(() =>
+//   import("./pages/Home/Products/Woman/ProductDetailsWoman")
+// );
+// const AddProduct = React.lazy(() =>
+//   import("./pages/Home/Products/Man/AddProductMan")
+// );
+// const AddProductWomen = React.lazy(() =>
+//   import("./pages/Home/Products/Woman/AddProductWoman")
+// );
+
+// const Games = React.lazy(() => import("./pages/Games/Games/Game"));
+// const Privacy = React.lazy(() => import("./pages/Games/Games/Privacy"));
+// const Terms = React.lazy(() => import("./pages/Games/Games/Terms"));
+// const About = React.lazy(() => import("./pages/Games/Games/About"));
+// const Contact = React.lazy(() => import("./pages/Games/Games/Contact"));
+
+// function App() {
+//   return (
+//     <div>
+//       <div>
+//         <GoogleTagManager />
+//       </div>
+//       <Routes>
+//         <Route
+//           path="/"
+//           element={
+//             <Suspense fallback={<Loader />}>
+//               <TimeLocationChecker />
+//             </Suspense>
+//           }
+//         />
+//         <Route path="/" element={<Home />} />
+//         <Route
+//           path="/kisi-etirleri"
+//           element={
+//             <Suspense fallback={<Loader />}>
+//               <ProductList />
+//             </Suspense>
+//           }
+//         />
+//         <Route
+//           path="/kisi-etirleri/:productName"
+//           element={
+//             <Suspense fallback={<Loader />}>
+//               <ProductDetails />
+//             </Suspense>
+//           }
+//         />
+//         <Route
+//           path="/qadin-etirleri"
+//           element={
+//             <Suspense fallback={<Loader />}>
+//               <ProductListWomen />
+//             </Suspense>
+//           }
+//         />
+//         <Route
+//           path="/qadin-etirleri/:productName"
+//           element={
+//             <Suspense fallback={<Loader />}>
+//               <ProductDetailsWomen />
+//             </Suspense>
+//           }
+//         />
+//         <Route
+//           path="/add"
+//           element={
+//             <Suspense fallback={<Loader />}>
+//               <AddProduct />
+//             </Suspense>
+//           }
+//         />
+//         <Route
+//           path="/addwomen"
+//           element={
+//             <Suspense fallback={<Loader />}>
+//               <AddProductWomen />
+//             </Suspense>
+//           }
+//         />
+//         <Route
+//           path="/1"
+//           element={
+//             <Suspense fallback={<Loader />}>
+//               <Games />
+//             </Suspense>
+//           }
+//         />
+//         <Route
+//           path="/1/tietosuoja"
+//           element={
+//             <Suspense fallback={<Loader />}>
+//               <Privacy />
+//             </Suspense>
+//           }
+//         />
+//         <Route
+//           path="/1/käyttöehdot"
+//           element={
+//             <Suspense fallback={<Loader />}>
+//               <Terms />
+//             </Suspense>
+//           }
+//         />
+//         <Route
+//           path="/1/noin"
+//           element={
+//             <Suspense fallback={<Loader />}>
+//               <About />
+//             </Suspense>
+//           }
+//         />
+//         <Route
+//           path="/1/otayhteyttä"
+//           element={
+//             <Suspense fallback={<Loader />}>
+//               <Contact />
+//             </Suspense>
+//           }
+//         />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Helmet } from "react-helmet";
@@ -5,6 +170,7 @@ import Home from "./pages/Home/Home";
 import Loader from "./components/Loader"; // Заглушка для Suspense
 import "./css/styles.scss";
 
+// Компонент для Google Tag Manager
 const GoogleTagManager = () => (
   <Helmet>
     {/* Добавляем скрипт Google Tag Manager */}
@@ -23,7 +189,14 @@ const GoogleTagManager = () => (
   </Helmet>
 );
 
-// Загружаем компоненты лениво
+// Компонент для обертки ленивой загрузки
+const LazyWrapper = ({ Component }) => (
+  <Suspense fallback={<Loader />}>
+    <Component />
+  </Suspense>
+);
+
+// Ленивая загрузка компонентов
 const TimeLocationChecker = React.lazy(() =>
   import("./components/TimeLocationChecker")
 );
@@ -45,7 +218,6 @@ const AddProduct = React.lazy(() =>
 const AddProductWomen = React.lazy(() =>
   import("./pages/Home/Products/Woman/AddProductWoman")
 );
-
 const Games = React.lazy(() => import("./pages/Games/Games/Game"));
 const Privacy = React.lazy(() => import("./pages/Games/Games/Privacy"));
 const Terms = React.lazy(() => import("./pages/Games/Games/Terms"));
@@ -59,102 +231,56 @@ function App() {
         <GoogleTagManager />
       </div>
       <Routes>
+        {/* Проверка местоположения и времени */}
         <Route
           path="/"
-          element={
-            <Suspense fallback={<Loader />}>
-              <TimeLocationChecker />
-            </Suspense>
-          }
+          element={<LazyWrapper Component={TimeLocationChecker} />}
         />
+
+        {/* Главная страница */}
         <Route path="/" element={<Home />} />
+
+        {/* Продукты для мужчин */}
         <Route
           path="/kisi-etirleri"
-          element={
-            <Suspense fallback={<Loader />}>
-              <ProductList />
-            </Suspense>
-          }
+          element={<LazyWrapper Component={ProductList} />}
         />
         <Route
           path="/kisi-etirleri/:productName"
-          element={
-            <Suspense fallback={<Loader />}>
-              <ProductDetails />
-            </Suspense>
-          }
+          element={<LazyWrapper Component={ProductDetails} />}
         />
+
+        {/* Продукты для женщин */}
         <Route
           path="/qadin-etirleri"
-          element={
-            <Suspense fallback={<Loader />}>
-              <ProductListWomen />
-            </Suspense>
-          }
+          element={<LazyWrapper Component={ProductListWomen} />}
         />
         <Route
           path="/qadin-etirleri/:productName"
-          element={
-            <Suspense fallback={<Loader />}>
-              <ProductDetailsWomen />
-            </Suspense>
-          }
+          element={<LazyWrapper Component={ProductDetailsWomen} />}
         />
-        <Route
-          path="/add"
-          element={
-            <Suspense fallback={<Loader />}>
-              <AddProduct />
-            </Suspense>
-          }
-        />
+
+        {/* Добавление продуктов */}
+        <Route path="/add" element={<LazyWrapper Component={AddProduct} />} />
         <Route
           path="/addwomen"
-          element={
-            <Suspense fallback={<Loader />}>
-              <AddProductWomen />
-            </Suspense>
-          }
+          element={<LazyWrapper Component={AddProductWomen} />}
         />
-        <Route
-          path="/1"
-          element={
-            <Suspense fallback={<Loader />}>
-              <Games />
-            </Suspense>
-          }
-        />
+
+        {/* Страницы для Финляндии */}
+        <Route path="/1" element={<LazyWrapper Component={Games} />} />
         <Route
           path="/1/tietosuoja"
-          element={
-            <Suspense fallback={<Loader />}>
-              <Privacy />
-            </Suspense>
-          }
+          element={<LazyWrapper Component={Privacy} />}
         />
         <Route
           path="/1/käyttöehdot"
-          element={
-            <Suspense fallback={<Loader />}>
-              <Terms />
-            </Suspense>
-          }
+          element={<LazyWrapper Component={Terms} />}
         />
-        <Route
-          path="/1/noin"
-          element={
-            <Suspense fallback={<Loader />}>
-              <About />
-            </Suspense>
-          }
-        />
+        <Route path="/1/noin" element={<LazyWrapper Component={About} />} />
         <Route
           path="/1/otayhteyttä"
-          element={
-            <Suspense fallback={<Loader />}>
-              <Contact />
-            </Suspense>
-          }
+          element={<LazyWrapper Component={Contact} />}
         />
       </Routes>
     </div>
